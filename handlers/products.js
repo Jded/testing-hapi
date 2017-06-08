a08af21e0cb6dab46eb938848b9901316e4e7fe3
Jded <jededor@gmail.com>
2017-06-08 19:50:03 +0100

    trigger test run

--- handlers/products.js
+++ handlers/products.js
@@ -6,7 +6,7 @@ const _ = require('lodash');
 // handlers are exported back for use in hapi routes
 const Handlers = {};
 
-const Nothing3 = {};
+const Nothing4 = {};
 
 // Lib contains our business specific logic
 const Lib = {};
